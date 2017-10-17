6ea894e966a4b293c1756749fed8d56eeb071764
shadzig <32872014+shadzig@users.noreply.github.com>
2017-10-17 15:51:56 +0100

    pet changes 2.0

--- flask.main.py
+++ flask.main.py
@@ -1,26 +1,13 @@
 from flask import Flask,request, render_template, jsonify
-from pymongo import MongoClient
 import datatime
+
+LOCAL = False
 
 app = Flask(__name__)
 
-user = 'bow'
-password = 'bowbow'
-host = 'ds113925.mlab.com:13925'
-db_name = 'rainbow'
-
-uri = "mongodb://%s:%s@%s/%s" % (user, password, host, db_name)
-
-print(uri)
-
-client = MongoClients(uri)
-db = client[db_name]
-
-print(db.my_collection.find())
-
 @app.route("/")
 def page():
-    return render_template("index.html")
+    return "Hello World"
 
-if __name__=="__main__":
+if LOCAL and __name__=="__main__":
     app.run(debug=True)

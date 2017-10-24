ca0f6f5a85adb199ec5c1d420956ae01c529c424
shadzig <32872014+shadzig@users.noreply.github.com>
2017-10-24 11:23:06 +0100

    m lab content

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from flask import Flask,request, render_template, jsonify
 import datetime
 import os
 
+from  credentials import db_access
+
+host= "ds113925.mlab.com:13925"
+db_name = "rainbow"
+
+uri = "mongodb://%s:%s@%s/%s" % (db_access.user, db_access.passowrd, host, db_name)
+
+print (uri)
+
+client = MongoClient(uri)
+db = client[db_name]
+
+for x in db.my.posts.find():
+    print(x)
+
+
 ON_HEROKU = "ON_HEROKU" in os.environ
 
 app = Flask(__name__)

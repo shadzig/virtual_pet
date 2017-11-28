b8c4f1c15bddf7bd7030609622fe91b19b8a6bef
shadzig <32872014+shadzig@users.noreply.github.com>
2017-11-28 15:23:22 +0000

    push

--- main.py
+++ main.py
@@ -10,30 +10,26 @@ db_name = "rainbow"
 
 uri = "mongodb://%s:%s@%s/%s" % (db_access.user, db_access.password, host, db_name)
 
-print(uri)
+print (uri)
 
 client = MongoClient(uri)
 db = client[db_name]
+
+for x in db.my.posts.find():
+    print(x)
+
 
 ON_HEROKU = "ON_HEROKU" in os.environ
 
 app = Flask(__name__)
 
-@app.route('/js/<path:path>')
+@app.route("/js/<path:path>")
 def send_js(path):
-    return send_from_directory('js', path)
-
-@app.route('/exchange', methods=['POST'])
-def exchange():
-    json = request.get_json()
-    if json['data'] == 37:
-        return jsonify({ 'x' : 56, 'y' : [-200, 55], 'thirty_seven': 'YES'  })
-    else:
-        return jsonify({ 'x' : 56, 'y' : [-200, 55], 'z' : json['data']  })
+    return send_from_directory("js", path)
 
 @app.route("/")
 def page():
-    return render_template('index.html', data=db.my_posts.find())
+    return render_template("index.html", data=db.my_posts.find())
 
-if (not ON_HEROKU) and __name__ == "__main__":
+if (not ON_HEROKU) and __name__=="__main__":
     app.run(debug=True)

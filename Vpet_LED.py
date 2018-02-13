2bb8299989b27b707cbbfb44ce16e1f057deb2fe
michail dzigajev <m.dzigajev@students.rave.ac.uk>
2018-02-13 22:43:00 +0000

    neoPixel fix

--- Vpet_LED.py
+++ Vpet_LED.py
@@ -12,7 +12,84 @@ LED_PIN = 18 # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 5 # DMA channel to use for generating signal (try 5)
 LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
 
+'''green = Color(0,230,0)
+
+opt_parse()
+
+def colorWipe(strip, color, wait_ms):
+    numbOfPix = strip.numPixels()
+    for i in range(numbOfPix):
+        strip.setPixelColor(0, Color(0,0,0))
+        print(i)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+        
+#print(green)
+colorWipe(strip, green, 20)
+
+'''
+
+smileLED = {
+    'neoNumbers' : [2,3,4,5,6,7,8,9,10,11,12,13,14],
+    'color' : {'g': 235,'r': 16, 'b' : 0}
+}
+
+neutralLED = {
+    'neoNumbers' : [2,3,4,7,8,9,10,11,12,13,14],
+    'color' : {'g': 235,'r': 235, 'b' : 0}
+    
+}
+sadLED = {
+    'neoNumbers' : [0,1,2,3,4,7,8,11,12,15,16],
+    'color' : {'g': 150,'r': 235, 'b' : 0}
+}
+
+angryLED = {
+    'neoNumbers' : [0,1,2,3,4,5,6,7,8,10,11,12,13,15,16],
+    'color' : {'g': 16,'r': 235, 'b' : 0}
+}
+
+clearLED = {
+    'neoNumbers' : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+    'color' : {'g': 0,'r': 0, 'b' : 0}
+}
+
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+
+for i in clearLED['neoNumbers']:
+    strip.setPixelColorRGB(i, clearLED['color']['g'], clearLED['color']['r'], clearLED['color']['b'])
+    strip.show()
+    
+
+for i in smileLED['neoNumbers']:
+    strip.setPixelColorRGB(i, smileLED['color']['g'], smileLED['color']['r'], smileLED['color']['b'])
+    strip.show()
+    
+    
+strip.begin()
+
+for i in neutralLED['neoNumbers']:
+    strip.setPixelColorRGB(i, neutralLED['color']['g'], neutralLED['color']['r'], neutralLED['color']['b'])
+    strip.show()
+    
+strip.begin()
+
+for i in sadLED['neoNumbers']:
+    strip.setPixelColorRGB(i, sadLED['color']['g'], sadLED['color']['r'], sadLED['color']['b'])
+    
+strip.begin() 
+ 
+for i in angryLED['neoNumbers']:
+    strip.setPixelColorRGB(i, angryLED['color']['g'], angryLED['color']['r'], angryLED['color']['b'])
+    strip.show()
+    
+
+'''
 # Define functions which animate LEDs in various ways.
 def Smile-green(strip, color, wait_ms=5000):
 """Smile animation, show smiling face."""
@@ -40,10 +117,7 @@ strip.show()
 time.sleep(wait_ms/1000.0)
 
 
-# Create NeoPixel object with appropriate configuration.
-strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
-# Intialize the library (must be called once before other functions).
-strip.begin()
+
 
 print 'Press Ctrl-C to quit.'
 while True:
@@ -56,3 +130,4 @@ while True:
         sad-orange(strip, color):
         print ('angry animation, show angry face.')
         angry-red(strip, color):
+'''
--- Vpet_main.py
+++ Vpet_main.py
@@ -1,21 +1,57 @@
 # Button serving script: runs repeatedly
-
 from datetime import datetime
 import time
 import sys
 import RPi.GPIO as GPIO
 
+
 # Sound trigger library content
 from gpiozero import OutputDevice
 from time import sleep
 
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(9, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
+# Neopixel libraries
+from neopixel import *
+
+# LED strip configuration:
+LED_COUNT = 17 # Number of LED pixels.
+LED_PIN = 18 # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 5 # DMA channel to use for generating signal (try 5)
+LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
+
+
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
+
+# Strip LED Settings
+smileLED = {
+    'neoNumbers' : [2,3,4,5,6,7,8,9,10,11,12,13,14],
+    'color' : {'g': 235,'r': 16, 'b' : 0}
+}
+
+neutralLED = {
+    'neoNumbers' : [2,3,4,7,8,9,10,11,12,13,14],
+    'color' : {'g': 235,'r': 235, 'b' : 0}
+    
+}
+sadLED = {
+    'neoNumbers' : [0,1,2,3,4,7,8,11,12,15,16],
+    'color' : {'g': 150,'r': 235, 'b' : 0}
+}
+
+angryLED = {
+    'neoNumbers' : [0,1,2,3,4,5,6,7,8,10,11,12,13,15,16],
+    'color' : {'g': 16,'r': 235, 'b' : 0}
+}
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+
 # Start by reading the existing JSON file, if it exists.
-
-
 IDLE_DAYTIME = 1
 Smile = 2
 Neutral = 3
@@ -44,46 +80,77 @@ def sampleButton():
     else:
         return 0
 
-def stateForTine():
-    # Return IDLE, Smile, Neutral, Sad, Angry LED smiley, depending on time of day:
-    pass
-
 def middleOfDay():
     now = datetime.now()
     hour = now.hour
     return hour > 12 and hour < 13
+    strip.begin()
 
 def stateForTime():
+    # Return IDLE, Smile, Neutral, Sad, Angry LED smiley, depending on time of day:
     now = datetime.now()
     hour = now.hour
 
     if hour >= 20:
         return Smile
+        
+        # Sound "hello"
         out= OutputDevice(26 ,False)
         out.on()
         sleep(0.126)
         out.toggle()
         
+        # Run Neopixel "smileLED", green
+        strip.begin()
+        for i in smileLED['neoNumbers']:
+            strip.setPixelColorRGB(i, smileLED['color']['g'], smileLED['color']['r'], smileLED['color']['b'])
+            strip.show()
+        
     elif hour >= 21:
         return Neutral
+    
+        # Sound "Snore"
+        strip.begin()
         out= OutputDevice(19 ,False)
         out.on()
         sleep(0.126)
         out.toggle()
         
+         # Run Neopixel "neutralLED", green
+        strip.begin()
+        for i in neutralLED['neoNumbers']:
+            strip.setPixelColorRGB(i, neutralLED['color']['g'], neutralLED['color']['r'], neutralLED['color']['b'])
+            strip.show()
+        
     elif hour >= 22:
         return Sad
+    
+        # Sound "Whine"
         out= OutputDevice(13 ,False)
         out.on()
         sleep(0.126)
         out.toggle()
         
+         # Run Neopixel "sadLED", green
+        strip.begin()
+        for i in sadLED['neoNumbers']:
+            strip.setPixelColorRGB(i, sadLED['color']['g'], sadLED['color']['r'], sadLED['color']['b'])
+            strip.show()
+        
     elif hour >= 23:
         return Angry
+    
+        # Sound "Grunt"
         out= OutputDevice(6 ,False)
         out.on()
         sleep(0.126)
         out.toggle()
+        
+         # Run Neopixel "angryLED", green
+        strip.begin()
+        for i in angryLED['neoNumbers']:
+            strip.setPixelColorRGB(i, angryLED['color']['g'], angryLED['color']['r'], angryLED['color']['b'])
+            strip.show()
         
     else:
         return IDLE_DAYTIME

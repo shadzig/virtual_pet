fe8bc8c1aae98c31623a98da0597af25e7a66b42
michail dzigajev <m.dzigajev@students.rave.ac.uk>
2018-02-19 21:00:43 +0000

    update for summ

---
+++ trigger_sound_test.py
@@ -0,0 +1,21 @@
+from gpiozero import OutputDevice
+from time import sleep
+from datetime import datetime
+
+
+pet_sound_repeat = datetime.now()
+
+out= OutputDevice(19,False)
+
+while True:
+
+    now = datetime.now()
+    SoundSecs = (now - pet_sound_repeat).total_seconds()
+
+    if SoundSecs > 4:
+        pet_sound_repeat = now
+
+        out.on()
+        sleep(0.126)
+        out.toggle()
+

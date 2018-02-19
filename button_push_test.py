a549f27e7b64175001243a7ff42dc7829cf545b7
michail dzigajev <m.dzigajev@students.rave.ac.uk>
2018-02-19 21:43:31 +0000

    push

--- button_push_test.py
+++ button_push_test.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(19)
+    input_state = GPIO.input(10)
     if input_state == False:
         print("button Pressed")
     time.sleep(0.2)
